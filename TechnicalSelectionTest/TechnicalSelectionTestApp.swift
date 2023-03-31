3701a08d05ef2ea4c5522ff2a3b111a58b1f0e06
Eduardo Herrera <edauardodesarrollador@icloud.com>
2023-03-31 13:47:23 +0200

    Initial Commit

---
+++ TechnicalSelectionTest/TechnicalSelectionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicalSelectionTestApp.swift
+//  TechnicalSelectionTest
+//
+//  Created by Eduardo Herrera on 31/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TechnicalSelectionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
